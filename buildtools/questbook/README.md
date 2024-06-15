ae0c5b820c47bef6fa2486a80d68c051e9ca2bb2
tier940 <tier940@protonmail.com>
2024-06-15 09:21:48 +0900

    Add bqu builder

--- buildtools/questbook/README.md
+++ buildtools/questbook/README.md
@@ -2,10 +2,10 @@
 ## How to translate
 
 1. First, you need to download the [latest version](https://github.com/GTModpackTeam/GTE2-Translations/releases)
-2. Download the `GTExpert2-<modpack_version>-ftbquests-<questbook_version>.zip`
+2. Download the `DefaultQuests_lang.json`
 3. Open the GTE2 instance folder
-4. Remove the `normal` folder from the `config/ftbquests` folder
-5. Unzip the `normal` in the downloaded zip file into the `config/ftbquests` folder
+4. Remove the `DefaultQuests.json` file from the `config/betterquesting` folder
+5. Download the `DefaultQuests_lang.json` file to the `config/betterquesting` folder and rename it to `DefaultQuests.json`
 6. Run the instance and enjoy the game!
 
 ## TODO
---
+++ buildtools/questbook/bak_ftbq/translate.py
@@ -0,0 +1,84 @@
+import deepl
+import re
+import os
+
+source_path = "ftbquests"
+target_path = "ftbquests-tl-" + os.environ["TARGET_LANG_CODE"]
+
+auth_key = os.environ["DEEPL_AUTH_KEY"]
+
+# Language code can be known in https://www.deepl.com/docs-api/general/get-languages
+target_lang_code = os.environ["TARGET_LANG_CODE"]
+
+# In some area on this earth, you should set a proper api server site at first.
+server_url = "https://api-free.deepl.com"
+
+tl = translator = deepl.Translator(auth_key, server_url=server_url)
+
+# this matches every "" sequence that contains a non-ascii unicode character
+# also accounts for escaped \" groups inside the double quotes
+match_jap = r'"((?:\\.|[^\\"\n\r])*)[\u0080-\uffff]((?:\\.|[^\\"\n\r])*)"'
+
+
+# replace matches of the r regex in s with f(match)
+def regex_replace(r, s, f):
+    new = ""
+    start = 0
+    for m in re.finditer(r, s):
+        end, newstart = m.span()
+        new += s[start:end]
+        new += f(m.group())
+        start = newstart
+    new += s[start:]
+    return new
+
+
+def copy_structure(src, dst):
+    for dirpath, dirnames, _ in os.walk(src):
+        for d in dirnames:
+            os.makedirs(
+                os.path.join(dst, *dirpath.split(os.path.sep)[1:], d),
+                exist_ok=True,
+            )
+
+
+def translate(s):
+    # un-escape the string so that it doesnt confuse deepl
+    res = s[1:-1].replace('\\"', '"')
+    res = tl.translate_text(
+        res, source_lang="JA", target_lang=target_lang_code , preserve_formatting=True
+    ).text
+    return '"' + res.replace('"', '\\"') + '"'
+
+
+def create_file_set(path, f=lambda s: s.endswith(".snbt")):
+    res = set()
+    for dirpath, _, filenames in os.walk(path):
+        for filename in filenames:
+            if not f(filename):
+                continue
+            file = os.path.join(dirpath, filename)
+            if os.path.isfile(file):
+                res.add(os.path.join(*file.split(os.path.sep)[1:]))
+    return res
+
+
+def main():
+    copy_structure(source_path, target_path)
+    ofiles = create_file_set(source_path)
+    tlfiles = create_file_set(target_path)
+    to_translate = ofiles - tlfiles
+    if not to_translate:
+        print("translation seems up to date")
+        return
+    for fp in to_translate:
+        with open(os.path.join(source_path, fp), "r", encoding='utf-8') as f:
+            s = f.read()
+            print(f"translating {fp}...")
+            translated = regex_replace(match_jap, s, translate)
+            with open(os.path.join(target_path, fp), "x", encoding='utf-8') as tf:
+                tf.write(translated)
+    print("done")
+
+
+main()
